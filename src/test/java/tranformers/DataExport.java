107fe3badf1996460b713a73666fb807846fd3be
joker1618 <federicobarbano@gmail.com>
2019-06-04 18:01:00 +0100

    fix

--- src/test/java/tranformers/DataExport.java
+++ src/test/java/tranformers/DataExport.java
@@ -5,11 +5,13 @@ import xxx.joker.apps.wrc.bomber.dl.WrcRepo;
 import xxx.joker.apps.wrc.bomber.dl.WrcRepoImpl;
 import xxx.joker.apps.wrc.bomber.dl.entities.FifaMatch;
 import xxx.joker.apps.wrc.bomber.dl.entities.WrcMatch;
+import xxx.joker.apps.wrc.bomber.dl.entities.WrcRally;
 import xxx.joker.apps.wrc.bomber.dl.entities.WrcSeason;
 import xxx.joker.libs.core.files.JkFiles;
 import xxx.joker.libs.core.format.JkOutput;
 import xxx.joker.libs.core.lambdas.JkStreams;
 import xxx.joker.libs.core.utils.JkStrings;
+import xxx.joker.libs.core.utils.JkStruct;
 import xxx.joker.libs.repository.util.RepoUtil;
 
 import java.nio.file.Paths;
@@ -39,8 +41,10 @@ public class DataExport {
         //NATION|SEASON ID|RALLY ID|STAGE PROGR IN RALLY|WINNER|CAR FEDE|CAR BOMBER|WEATHER|TIME|CREATION TM
         List<String> lines = new ArrayList<>();
         Map<Long, WrcSeason> seasonMap = JkStreams.toMapSingle(repo.getSeasons(), WrcSeason::getEntityID);
-        lines.add("NATION|SEASON ID|RALLY ID|STAGE PROGR IN RALLY|WINNER|CAR FEDE|CAR BOMBER|WEATHER|TIME|CREATION TM|ENTITY ID|SEASON FINISHED|SEASON START");
+//        lines.add("NATION|SEASON ID|RALLY ID|STAGE PROGR IN RALLY|WINNER|CAR FEDE|CAR BOMBER|WEATHER|TIME|CREATION TM|ENTITY ID|SEASON FINISHED");
+        lines.add("NATION|SEASON ID|RALLY ID|STAGE PROGR IN RALLY|WINNER|CAR FEDE|CAR BOMBER|WEATHER|TIME|CREATION TM|ENTITY ID|SEASON FINISHED|SEASON START|SEASON END|RALLY WINNER|SEASON WINNER");
         for (WrcMatch match : repo.getMatches()) {
+            WrcSeason s = seasonMap.get(match.getSeasonID());
             List<String> row = new ArrayList<>();
             row.add(match.getNation().getName());
             row.add(match.getSeasonID()+"");
@@ -53,11 +57,14 @@ public class DataExport {
             row.add(match.getTime().name());
             row.add(match.getCreationTm().format());
             row.add(match.getEntityID()+"");
-            row.add(seasonMap.get(match.getSeasonID()).isFinished()+"");
-            row.add(seasonMap.get(match.getSeasonID()).getCreationTm().format());
+            row.add(s.isFinished()+"");
+            row.add(s.getCreationTm().format());
+            row.add(s.isFinished() ? JkStruct.getLastElem(JkStruct.getLastElem(s.getRallyList()).getMatches()).getCreationTm().format() : "");
+            row.add(((WrcRally)repo.getById(match.getRallyID())).getWinner().name());
+            row.add(s.isFinished() ? s.getWinner().name() : "");
             lines.add(JkStreams.join(row, "|"));
         }
-        JkFiles.writeFile(Paths.get("csvExport/csvMigration.csv"), lines);
+        JkFiles.writeFile(Paths.get("csvExport/csvMatchesMigration.csv"), lines);
     }
 
     @Test
