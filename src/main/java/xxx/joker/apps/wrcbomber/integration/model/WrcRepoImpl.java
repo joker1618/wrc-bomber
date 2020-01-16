1bb4890dab4d398fa187a31d619ea2147b5a5a5e
joker1618 <federicobarbano@gmail.com>
2020-01-16 23:48:06 +0000

    fix

--- src/main/java/xxx/joker/apps/wrcbomber/integration/model/WrcRepoImpl.java
+++ src/main/java/xxx/joker/apps/wrcbomber/integration/model/WrcRepoImpl.java
@@ -3,16 +3,31 @@ package xxx.joker.apps.wrcbomber.integration.model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import xxx.joker.apps.wrcbomber.config.AppConfig;
+import xxx.joker.apps.wrcbomber.dl.entities.wrc.*;
+import xxx.joker.libs.core.runtime.JkReflection;
 import xxx.joker.libs.repo.JkRepo;
 import xxx.joker.libs.repo.JkRepoFile;
 import xxx.joker.libs.repo.design.SimpleRepoEntity;
 
 import java.nio.file.Path;
+import java.util.Arrays;
 
 public class WrcRepoImpl extends JkRepoFile implements WrcRepo {
 
     public WrcRepoImpl(Path repoFolder, String dbName) {
-        super(repoFolder, dbName, "xxx.joker.apps.wrcbomber.dl.entities");
+        super(repoFolder, dbName, Arrays.asList(
+                WrcCar.class,
+                WrcCountry.class,
+                WrcGroundMix.class,
+                WrcGroundType.class,
+                WrcSurface.class,
+                WrcMatch.class,
+                WrcRally.class,
+                WrcSeason.class,
+                WrcStage.class,
+                WrcWeather.class,
+                WrcRaceTime.class
+        ));
     }
 
 
